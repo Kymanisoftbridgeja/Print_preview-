896008a520bad5e5b0fe37037722cea6cb3add2f
Kymani Edwards <johvaine@softbridgeja.com>
2026-05-28 10:44:13 -0400

    Removed print preview applications

--- App/app/src/main/java/com/receiptbridge/escpos/PrinterConnection.kt
+++
@@ -1,8 +0,0 @@
-package com.receiptbridge.escpos
-
-interface PrinterConnection {
-    suspend fun connect()
-    suspend fun disconnect()
-    suspend fun write(data: ByteArray)
-    fun isConnected(): Boolean
-}
--- OdooModule/softbridge_pos_restaurant_bridge/models/pos_config.py
+++
@@ -1,5 +0,0 @@
-from odoo import models
-
-
-class PosConfig(models.Model):
-    _inherit = "pos.config"
